fd163c6a26b799c86b4ea07a85bfc0f044bd40df
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2018-02-28 14:46:52 +0100

    Normalized Map component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,30 @@
-import { GoogleMap, Marker } from 'react-google-maps'
+import React from 'react'
+import { compose, withProps } from 'recompose'
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 
-const Map = (props) =>
+const MapElement = compose(
+    withProps({
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+        loadingElement: <div style={{ height: `100%` }} />,
+        containerElement: <div style={{ height: `400px` }} />,
+        mapElement: <div style={{ height: `100%` }} />,
+    }),
+    withScriptjs,
+    withGoogleMap
+)((props) =>
     <GoogleMap
         defaultZoom={8}
         defaultCenter={{ lat: -34.397, lng: 150.644 }} >
         {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
     </GoogleMap>
+)
+
+export default class Map extends React.PureComponent {
+    state = { isMarkerShown: true }
+    componentDidMount() {}
+    render() {
+        return (
+            <MapElement isMarkerShown={this.state.isMarkerShown} />
+        )
+    }
+}
