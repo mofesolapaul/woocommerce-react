7789784c7637608c427950ed8638ba9fcaf779e7
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2018-03-01 09:01:14 +0100

    Added ETALabel component

---
+++ src/components/ETALabel.js
@@ -0,0 +1,34 @@
+export default props => (
+    <div className="ETA">
+        <h5>ETA</h5>
+
+        {/* styles */}
+        <style jsx>{`
+            @keyframes drawUp {
+                to { bottom: 0 }
+            }
+            @keyframes drop {
+                to { top: 0 }
+            }
+            .ETA {
+                display: ${props.shown? 'block':'none'};
+                position: absolute;
+                left: 0; right: 0;
+                margin: auto;
+                padding: .5rem;
+            }
+            @media screen and (max-width: 719px) {
+                .ETA {
+                    bottom: -100%;
+                    animation: drawUp .25s ease-in-out;
+                }
+            }
+            @media screen and (min-width: 720px) {
+                .ETA {
+                    top: -100%;
+                    animation: drop .25s ease-in-out;
+                }
+            }
+        `}</style>
+    </div>
+)
