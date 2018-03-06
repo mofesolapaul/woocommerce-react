7555e4075682b3383d4ce8b86b6f6ca2aedc9164
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2018-03-06 16:10:44 +0100

    Ignore config.js

--- src/WooCommerce/Config.js
+++ src/WooCommerce/Config.js
@@ -7,7 +7,7 @@ const Constants = {
     Keys: {
         ConsumerKey: 'ck_6724bbb976aa5f2b9ef546b63b820c06a374412d',
 	    ConsumerSecret: 'cs_ce70404e60bee2919a7861ae50675d7df1b29dc9'
-    }
+    },
 }
 
 module.exports = Constants;
