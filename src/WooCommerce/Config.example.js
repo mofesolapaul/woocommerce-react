be99676554334866e7d55884534c0e6e836b008e
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2018-03-06 16:13:23 +0100

    Show paystack config example

--- src/WooCommerce/Config.example.js
+++ src/WooCommerce/Config.example.js
@@ -7,7 +7,13 @@ const Constants = {
     Keys: {
         ConsumerKey: 'ck_blahblahblah',
 	    ConsumerSecret: 'cs_blahblahblah'
-    }
+    },
+    Paystack: {
+        TestPublicKey: 'pk_test_blahblah',
+        TestSecretKey: 'sk_test_blahblah',
+        LivePublicKey: 'pk_live_blahblah',
+        LiveSecretKey: 'sk_live_blahblah',
+    },
 }
 
 module.exports = Constants;
