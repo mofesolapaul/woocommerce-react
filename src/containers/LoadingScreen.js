3637475733a1501d84257b0ac7b2d0c77f9f11ae
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2018-03-08 09:42:35 +0100

    Added LoadingScreen component

---
+++ src/containers/LoadingScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import css from '../../styles/vars'
+import {Loading} from '../components'
+
+class LoadingScreen extends React.Component {
+    render() {
+        <div className="LoadingScreen flex">
+            <Loading />
+
+            {/* styles */}
+            <style jsx>{`
+            `}</style>
+        </div>
+    }
+}
