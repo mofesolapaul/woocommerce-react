a63a28974aae72f9e1e5eeb67242e19c016d3af8
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2018-03-12 14:54:04 +0100

    Minor fix

--- src/components/PaystackButton.js
+++ src/components/PaystackButton.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PaystackButton from 'react-paystack'
 import Head from 'next/head'
 import { View } from '.'
+import {URL} from '../Config'
 
 export default class _default extends React.Component {
     constructor(props) {
@@ -13,7 +14,7 @@ export default class _default extends React.Component {
     render() {
         return <View>
             <Head key="paystack-script">
-                <script async src="/static/js/paystack.inline.min.js"></script>
+                <script async src={`${URL.base}static/js/paystack.inline.min.js`}></script>
             </Head>
             <PaystackButton {...this.props} ref={btn => this.innerBtn = btn} />
         </View>
