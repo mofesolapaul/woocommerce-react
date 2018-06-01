c8e732b8f3209568125864e1d7c89be088096ac9
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2018-06-01 14:21:40 +0100

    ProductFilters styled up

--- src/components/ProductFilters.js
+++ src/components/ProductFilters.js
@@ -1,12 +1,14 @@
 import {View} from '.';
 import {CATEGORIES} from '../constants';
+import css from '../../styles/vars';
 
 export default props => <View>
     <div className="ProductFilters">
         <div className="pf--group">
             Showing
-            <select className="pr--list">
+            <select className="pf--list">
                 <option value="">Everything</option>
+                {CATEGORIES.map((c,k) => <option key={k} value={c}>{c}</option>)}
             </select>
         </div>
     </div>
@@ -17,8 +19,22 @@ export default props => <View>
             text-align: center;
             margin-bottom: 1rem;
         }
-        .pf--group {
+        .pf--group, .pf--list {
             text-transform: uppercase;
+        }
+        .pf--list {
+            -webkit-appearance: button;
+            margin: 0 .4rem;
+            position: relative;
+            padding: 4px 1rem;
+            font-weight: bold;
+            cursor: pointer;
+        }
+        .pf--list::after {
+            content: "\u2304";
+            position: absolute;
+            right: 2px;
+            display: block;
         }
     `}</style>
 </View>
