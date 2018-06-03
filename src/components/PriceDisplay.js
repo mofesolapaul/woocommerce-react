642ebd0ba5dc024f08beb5d41aaea272913fc776
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2018-06-03 11:13:13 +0100

    Style update on PriceDisplay

--- src/components/PriceDisplay.js
+++ src/components/PriceDisplay.js
@@ -19,7 +19,7 @@ export default ({price, qty}) => <View>
         font-size: larger;
         padding: .4rem .8rem;
         color: ${css.colors.background};
-        background: ${!qty? css.colors.foreground:'rgba(0,0,0,.9)'}
+        background: ${!qty? css.colors.foreground:css.colors.foreground_deep}
         padding-left: 1.2rem;
     }
     .qty {
