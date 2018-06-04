f5f00234dafcded07c1d04ca2ad1df0bdea68778
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2018-06-04 11:58:09 +0100

    Made CartButtons component stateless

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -3,70 +3,48 @@ import css from '../../styles/vars';
 import {View} from './';
 import {bindToThis} from '../constants';
 
-export default class CartButtons extends React.Component {
-    constructor(props) {
-        super(props);
+export default ({handler, solo}) => <View>
+    {!solo && <View>
+        <a className="btn cart-btn left" onClick={() => handler('cart.button.remove')}>{`\u2013`}</a>
+        <a className="btn cart-btn right" onClick={() => handler('cart.button.add')}>+</a>
+    </View>}
+    {!!solo && <a className="btn cart-btn solo-btn" onClick={() => handler('cart.button.solo_add')}>Add to cart</a>}
 
-        // bind
-        bindToThis(this, 'add');
-        bindToThis(this, 'solo_add');
-        bindToThis(this, 'remove');
+    {/* style */}
+    <style jsx>{`
+    .cart-btn {
+        width: 28px;
+        height: 28px;
+        line-height: 24px;
+        text-align: center; 
+        font-size: 16px;
+        transition: none;
+        padding: 0;
     }
-    add() {
-        this.props.handler && this.props.handler('cart.button.add');
+    .cart-btn:hover {
+        background: ${css.colors.primarydark};
+        color: ${css.colors.background};
+        border-color: transparent;
     }
-    solo_add() {
-        this.props.handler && this.props.handler('cart.button.solo_add');
+    .left {
+        border-right: none;
+        border-radius: 50% 0 0 50%;
     }
-    remove() {
-        this.props.handler && this.props.handler('cart.button.remove');
+    .right {
+        border-radius: 0 50% 50% 0;
     }
-    render() {
-        return <View>
-            {!this.props.solo && <View>
-                <a className="btn cart-btn left" onClick={this.remove}>{`\u2013`}</a>
-                <a className="btn cart-btn right" onClick={this.add}>+</a>
-            </View>}
-
-            {!!this.props.solo && <a className="btn cart-btn solo-btn" onClick={this.solo_add}>Add to cart</a>}
-
-            {/* style */}
-            <style jsx>{`
-            .cart-btn {
-                width: 28px;
-                height: 28px;
-                line-height: 24px;
-                text-align: center; 
-                font-size: 16px;
-                transition: none;
-                padding: 0;
-            }
-            .cart-btn:hover {
-                background: ${css.colors.primarydark};
-                color: ${css.colors.background};
-                border-color: transparent;
-            }
-            .left {
-                border-right: none;
-                border-radius: 50% 0 0 50%;
-            }
-            .right {
-                border-radius: 0 50% 50% 0;
-            }
-            .cart-btn:active {
-                background: ${css.colors.primarydark};
-                box-shadow: rgba(82,89,101,.25) 1px 2px 6px inset;
-                user-select: none;
-            }
-            .solo-btn {
-                width: auto;
-                border-radius: 20px;
-                text-transform: uppercase;
-                font-size: smaller;
-                padding: 2px 6px;
-                font-weight: bolder;
-            }
-            `}</style>
-        </View>;
+    .cart-btn:active {
+        background: ${css.colors.primarydark};
+        box-shadow: rgba(82,89,101,.25) 1px 2px 6px inset;
+        user-select: none;
     }
-}
+    .solo-btn {
+        width: auto;
+        border-radius: 20px;
+        text-transform: uppercase;
+        font-size: smaller;
+        padding: 2px 6px;
+        font-weight: bolder;
+    }
+    `}</style>
+</View>;
