fc3c469163c795ea47be131b2ab5d25da0f34eb7
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2018-06-09 08:25:21 +0100

    Added support for no-shadow in Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -10,6 +10,6 @@ export const ButtonPane = props => <div className="show-more-pane">
 
 // show more
 export default (props) =>
-    <a onClick={!!props.finished? null:props.clickHandler} className={`btn sleek-btn ${!!props.finished? 'dead':''} ${props.className || ''}`}>
+    <a onClick={!!props.finished? null:props.clickHandler} className={`btn sleek-btn ${!!props.finished? 'dead':''} ${props.className || ''} ${props['no-shadow'] && 'no-shadow'}`}>
         {!!props.finished? "Yup, that's all!" : (props.label || 'Show more')}
     </a>
